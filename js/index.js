697da5fc3d8325fdc5219146da89493674fc02d8
minasoha <minasoha@yahoo.com>
2021-08-31 19:09:51 -0400

    finished 2 types

--- js/index.js
+++ js/index.js
@@ -1,1 +1,49 @@
 // Your code goes here
+const aTags = document.querySelectorAll('.nav a');
+const logo = document.querySelector('header.logo-heading');
+const funPicture = document.querySelectorAll('.img-content');
+const signUp = document.querySelectorAll('.btn');
+const welcome = document.querySelector('.intro h2');
+
+
+
+aTags.forEach(aTags =>{
+    aTags.addEventListener('mouseenter', function(event){
+        event.target.style.color = 'green';
+    
+        setTimeout(function(){
+            event.target.style.color = '';
+        },500);
+    }, false);
+});
+
+funPicture.forEach(funPicture => {
+    funPicture.addEventListener('click', function(event){
+        event.target.style.borderRadius = '0';
+
+        setTimeout(function(){
+            event.target.style.borderRadius = '10px';
+        }, 500);
+    }, false);
+});
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+    logo.textContent += ` ${e.code}`;
+  };
+
+signUp.forEach(signUp =>{
+    signUp.addEventListener('mouseleave', event =>{
+       event.target.style.color = 'purple';
+        event.target.style.color = '';
+  }, 1000);
+ }, false);
+
+ function confirm(event){
+    welcome.classList.remove('.intro');
+    logo.classList.add('.logo-heading')
+
+ }
+
+ welcome.addEventListener('mouseenter', confirm);
